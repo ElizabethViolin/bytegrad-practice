710eae556ed76d841d0674d8b60066941b9e7393
ElizabethViolin <elizabethtaylorjoy@gmail.com>
2024-06-21 19:01:09 +0800

    complete UI and component structure

---
+++ project-2/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <small>&copy; Copyright by ByteGrad. All rights reserved.</small>
+      <small>Last checked limits: 17 days ago</small>
+    </div>
+  );
+}
---
+++ project-2/src/components/Stats.jsx
@@ -0,0 +1,22 @@
+export default function Stats() {
+  return (
+    <div className="stats">
+      <div className="stat">
+        <h2 className="stat__number">0</h2>
+        <span className="second-heading">Words</span>
+      </div>
+      <div className="stat">
+        <h2 className="stat__number">0</h2>
+        <span className="second-heading">Characters</span>
+      </div>
+      <div className="stat">
+        <h2 className="stat__number">280</h2>
+        <span className="second-heading">Instagram</span>
+      </div>
+      <div className="stat">
+        <h2 className="stat__number">2200</h2>
+        <span className="second-heading">Facebook</span>
+      </div>
+    </div>
+  );
+}
