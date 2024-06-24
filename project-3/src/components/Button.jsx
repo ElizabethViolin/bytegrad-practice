2d6979a489540f8d638c94402aefee8c1cee7b32
ElizabethViolin <elizabethtaylorjoy@gmail.com>
2024-06-24 16:22:13 +0800

    complete functionality for secondary buttons

--- project-3/src/components/Button.jsx
+++ project-3/src/components/Button.jsx
@@ -1,7 +1,8 @@
-export default function Button({ buttonType, children }) {
+export default function Button({ buttonType, children, onClick }) {
   return (
     <button
       className={`btn ${buttonType === "secondary" ? "btn--secondary" : ""}`}
+      onClick={onClick}
     >
       {children}
     </button>
--- project-3/src/components/ButtonGroup.jsx
+++ project-3/src/components/ButtonGroup.jsx
@@ -1,12 +1,25 @@
 import Button from "./Button";
 
-export default function ButtonGroup() {
+export default function ButtonGroup({
+  handleRemoveAllItems,
+  handleResetToInitial,
+  handleMarkAllAsComplete,
+  handleMarkAllAsIncomplete,
+}) {
   return (
     <section className="button-group">
-      <Button buttonType="secondary">Mark all as complete</Button>
-      <Button buttonType="secondary">Mark all as incomplete</Button>
-      <Button buttonType="secondary">Reset to initial</Button>
-      <Button buttonType="secondary">Remove all items</Button>
+      <Button onClick={handleMarkAllAsComplete} buttonType="secondary">
+        Mark all as complete
+      </Button>
+      <Button onClick={handleMarkAllAsIncomplete} buttonType="secondary">
+        Mark all as incomplete
+      </Button>
+      <Button onClick={handleResetToInitial} buttonType="secondary">
+        Reset to initial
+      </Button>
+      <Button onClick={handleRemoveAllItems} buttonType="secondary">
+        Remove all items
+      </Button>
     </section>
   );
 }
