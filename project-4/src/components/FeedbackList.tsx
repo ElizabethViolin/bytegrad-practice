674cbd5a11bee16458658979ad3abb2022286e06
ElizabethViolin <elizabethtaylorjoy@gmail.com>
2024-06-26 20:07:02 -0400

    basic UI and component structure, installed Radix UI icons

---
+++ project-4/src/components/FeedbackList.tsx
@@ -0,0 +1,29 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
+export default function FeedbackList() {
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>593</span>
+        </button>
+
+        <div>
+          <p>B</p>
+        </div>
+
+        <div>
+          <p>ByteGrad</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis impedit commodi cum consequuntur reprehenderit
+            accusantium!
+          </p>
+        </div>
+
+        <p>4d</p>
+      </li>
+    </ol>
+  );
+}
