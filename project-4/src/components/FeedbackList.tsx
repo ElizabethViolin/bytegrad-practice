3858ef87c616eddac26283c0163626deab9fb6e0
ElizabethViolin <elizabethtaylorjoy@gmail.com>
2024-06-28 15:49:15 -0400

    fetching data in FeedbackList

--- project-4/src/components/FeedbackList.tsx
+++ project-4/src/components/FeedbackList.tsx
@@ -1,27 +1,68 @@
+import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
-
-const feedbackItems = [
-  {
-    upvoteCount: 10,
-    badgeLetter: "B",
-    companyName: "ByteGrad",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis impedit commodi cum consequuntur reprehenderit accusantium!",
-    daysAgo: 4,
-  },
-  {
-    upvoteCount: 20,
-    badgeLetter: "C",
-    companyName: "Company",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis impedit commodi cum consequuntur reprehenderit accusantium!",
-    daysAgo: 2,
-  },
-];
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessages, setErrorMessages] = useState("");
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(
+  //     "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+  //   )
+  //     .then((res) => {
+  //       if (!res.ok) {
+  //         throw new Error();
+  //       }
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       setFeedbackItems(data.feedbacks);
+  //       setIsLoading(false);
+  //     })
+  //     .catch(() => {
+  //       setErrorMessages("Something went wrong.");
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        // network error, not 2xx response, or JSON parsing error
+        setErrorMessages("Something went wrong.");
+        setIsLoading(false);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+  }, []);
+
   return (
     <ol className="feedback-list">
+      {isLoading && <Spinner />}
+
+      {errorMessages && <ErrorMessage message={errorMessages} />}
+
       {feedbackItems.map((feedbackItem) => (
-        <FeedbackItem feedbackItem={feedbackItem} />
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
     </ol>
   );
